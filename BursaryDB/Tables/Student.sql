acc91c71a4e0d62ad473430e41fb15d2fcb94107
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-09 11:11:19 +0200

    fix email data type

--- BursaryDB/Tables/Student.sql
+++ BursaryDB/Tables/Student.sql
@@ -5,7 +5,7 @@ CREATE TABLE [dbo].[Student]
   [LastName] VARCHAR(50) NOT NULL,
   [AverageMark] INT NOT NULL,
   [ContactNumber] CHAR(10) NOT NULL,
-  [StudentEmail] CHAR(100) NOT NULL,
+  [StudentEmail] VARCHAR(100) NOT NULL,
   [DateOfBirth] DATE NOT NULL,
   [CONSTRAINT] PK_StudentID
     PRIMARY KEY CLUSTERED (StudentID)
