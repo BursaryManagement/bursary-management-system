667e66045a4065fc79a949a65255d9a100c0cda9
Bulelani Gabonewe <Bulelani.Gabonewe@bbd.co.za>
2024-02-09 10:14:54 +0200

    Create the First Table

---
+++ BursaryDB/Tables/University.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[University]
+(
+  [UniversityId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  [UniversityName] VARCHAR(120) NOT NULL,
+  [AmountAllocated] MONEY NOT NULL DEFAULT 0,
+  [ReamingAmount] MONEY  NOT NULL DEFAULT 0
+)
