9d9716ee03842434de485e1a230809c94af37801
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-09 11:31:17 +0200

    Added relationship between StudentBursaryApplication and StudentBursaryDocuments

--- BursaryDB/Tables/StudentBursaryDocuments.sql
+++ BursaryDB/Tables/StudentBursaryDocuments.sql
@@ -6,3 +6,8 @@ CREATE TABLE [dbo].[StudentBursaryDocuments]
   [CurriculumVitae] VARBINARY(MAX) NOT NULL,
   [ApplicationID] INT NOT NULL
 )
+
+ALTER TABLE [dbo].[StudentBursaryDocuments]
+  ADD CONSTRAINT [FK_StudentBursaryDocuments_StudentBursaryApplication_ApplicationID]
+  FOREIGN KEY (ApplicationID)
+  REFERENCES StudentBursaryApplication([ApplicationID])
