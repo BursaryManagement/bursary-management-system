c9f14801a1f6c2ecba107955e10e01379bf74fc4
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-09 14:06:22 +0200

    Added relationship constraints for table HeadOfDepartment

--- BursaryDB/Tables/HeadOfDepartment.sql
+++ BursaryDB/Tables/HeadOfDepartment.sql
@@ -3,5 +3,20 @@ CREATE TABLE [dbo].[HeadOfDepartment]
   [HeadOfDepartmentID] INT IDENTITY NOT NULL PRIMARY KEY,
   [DepartmentID] INT NOT NULL,
   [UniversityID] INT NOT NULL,
-  [UserID] INT NOT NULL
+  [UserID] INT NOT NULL UNIQUE
 )
+GO
+
+ALTER TABLE [dbo].[HeadOfDepartment]
+  ADD CONSTRAINT [FK_HeadOfDepartment_Department_DepartmentID] 
+      FOREIGN KEY ([DepartmentID]) 
+      REFERENCES [dbo].[Department]([DepartmentID]),
+
+      CONSTRAINT [FK_HeadOfDepartment_University_UniversityID]
+      FOREIGN KEY ([UniversityID])
+      REFERENCES [dbo].[University]([UniversityID]),
+
+      CONSTRAINT [FK_HeadOfDepartment_User_UserID]
+      FOREIGN KEY ([UserID])
+      REFERENCES [dbo].[User]([UserID])
+GO
