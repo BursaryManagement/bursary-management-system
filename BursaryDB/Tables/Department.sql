412a7232528df4caa9b4673b29ae5f112e841232
Bulelani Gabonewe <Bulelani.Gabonewe@bbd.co.za>
2024-02-09 13:24:48 +0200

    Fix ID name, use proper convetion

--- BursaryDB/Tables/Department.sql
+++ BursaryDB/Tables/Department.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[Department]
 (
-  [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  [DepartmentID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
   [DepartmentName] VARCHAR(100) NOT NULL
 )
