b00b8ef4cbe3501838fc1973e6c97cd4fd0e4f91
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-09 13:11:47 +0200

    Synchronizing branches

--- BursaryDB/Tables/HeadOfDepartment.sql
+++ BursaryDB/Tables/HeadOfDepartment.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[HeadOfDepartment]
 (
   [HeadOfDepartmentID] INT IDENTITY NOT NULL PRIMARY KEY,
-  [DepartmentID] INT,
-  [UniversityID] INT,
-  [UserID] INT
+  [DepartmentID] INT NOT NULL,
+  [UniversityID] INT NOT NULL,
+  [UserID] INT NOT NULL
 )
