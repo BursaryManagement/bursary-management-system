06c8eaddad474670d8ba0cfcf5eda457a2e921f6
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-09 14:19:44 +0200

    Added relationships for StudentBursaryApplication table

--- BursaryDB/Tables/StudentBursaryApplication.sql
+++ BursaryDB/Tables/StudentBursaryApplication.sql
@@ -4,10 +4,17 @@ CREATE TABLE [dbo].[StudentBursaryApplication]
   [ApplicationMotivation] TEXT NOT NULL,
   [ApplicationRejectionReason] TEXT DEFAULT '',
   [BursaryAmount] MONEY NOT NULL,
-  [StudentID] VARCHAR(13) NOT NULL
+  [StudentID] VARCHAR(13) NOT NULL UNIQUE,
+  [StatusID] INT NOT NULL UNIQUE
 )
+GO
 
 ALTER TABLE [dbo].[StudentBursaryApplication] 
   ADD CONSTRAINT [FK_StudentBursaryApplication_Student_StudentID]
-  FOREIGN KEY (StudentID)
-  REFERENCES Student([StudentID])
+      FOREIGN KEY ([StudentID])
+      REFERENCES [dbo].[Student]([StudentID]),
+
+      CONSTRAINT [FK_StudentBursaryApplication_ApplicationStatus_StatusID]
+      FOREIGN KEY ([StatusID])
+      REFERENCES [dbo].[ApplicationStatus]([StatusID])
+GO
