36ebfb36f47d57a3a947570494bc5fe69839ff7c
Manqoba-stack <manyoba1997@gmail.com>
2024-02-10 14:24:30 +0200

    removed error causing attributes

--- BursaryDB/Tables/Student.sql
+++ BursaryDB/Tables/Student.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [dbo].[Student]
 (
-  [StudentID] VARCHAR(13) IDENTITY(1,1)  PRIMARY KEY NOT NULL,
+  [StudentID] INT IDENTITY(1,1)  PRIMARY KEY NOT NULL,
   [AverageMark] INT NOT NULL,
   [UserID] INT NOT NULL,
   [RaceID] INT NOT NULL, 
   [UniversityID] INT NOT NULL,
-  [DepartmentID] INT NOT NULL,
+  [DepartmentID] INT NOT NULL
 )
 GO
