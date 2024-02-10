37267e2665cefbefc81abe63a9a466e4c5d996e6
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-10 12:01:41 +0200

    recreate student table

--- BursaryDB/Tables/Student.sql
+++ BursaryDB/Tables/Student.sql
@@ -1,31 +1,10 @@
-IF object_id('Student') IS NULL 
-  CREATE TABLE [dbo].[Student]
-  (
-    [StudentID] VARCHAR(13) NOT NULL,
-    [FirstName] VARCHAR(50) NOT NULL,
-    [LastName] VARCHAR(50) NOT NULL,
-    [AverageMark] INT NOT NULL,
-    [ContactNumber] CHAR(10) NOT NULL,
-    [StudentEmail] VARCHAR(100) NOT NULL,
-    [DateOfBirth] DATE NOT NULL,
-    [RaceID] INT NOT NULL, 
-    [UniversityID] INT NOT NULL,
-    [DepartmentID] INT NOT NULL,
-    CONSTRAINT [PK_Student]
-      PRIMARY KEY CLUSTERED (StudentID)
-  )
-
-ALTER TABLE Student
-ADD CONSTRAINT [FK_StudentRace]
-FOREIGN KEY (RaceID)
-REFERENCES Race(RaceID)
-
-ALTER TABLE Student
-ADD CONSTRAINT [FK_StudentUniversity]
-FOREIGN KEY (UniversityID)
-REFERENCES University(UniversityID);
-
-ALTER TABLE Student
-ADD CONSTRAINT [FK_StudentDepartment]
-FOREIGN KEY (DepartmentID)
-REFERENCES Department(DepartmentID);
+CREATE TABLE [dbo].[Student]
+(
+  [StudentID] VARCHAR(13) IDENTITY(1,1)  PRIMARY KEY NOT NULL,
+  [AverageMark] INT NOT NULL,
+  [UserID] INT NOT NULL,
+  [RaceID] INT NOT NULL, 
+  [UniversityID] INT NOT NULL,
+  [DepartmentID] INT NOT NULL,
+)
+GO
