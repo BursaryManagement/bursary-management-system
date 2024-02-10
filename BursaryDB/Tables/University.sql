2cea652ee62bd779570c9fd4c80c46b2be212e2f
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-10 12:26:05 +0200

    add screipt to create university table

--- BursaryDB/Tables/University.sql
+++ BursaryDB/Tables/University.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [dbo].[University]
 (
   [UniversityID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-  [UniversityName] VARCHAR(120) NOT NULL,
-  [AmountAllocated] MONEY NOT NULL DEFAULT 0,
-  [RemainingAmount] MONEY  NOT NULL DEFAULT 0
-)
+  [UniversityName] VARCHAR(100) NOT NULL
+);
+GO
 
