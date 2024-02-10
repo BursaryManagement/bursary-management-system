801fd3e27ce97c17f9f5ec9f18eed1a332fd5b9a
Bulelani Gabonewe <53180071+gaboneweb@users.noreply.github.com>
2024-02-10 15:48:58 +0200

    Update StudentBursaryApplication.sql

--- BursaryDB/Tables/StudentBursaryApplication.sql
+++ BursaryDB/Tables/StudentBursaryApplication.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[StudentBursaryApplication]
   [ApplicationMotivation] TEXT NOT NULL,
   [ApplicationRejectionReason] TEXT NOT NULL DEFAULT 'N/A',
   [BursaryAmount] MONEY NOT NULL,
-  [StudentID] VARCHAR(13) NOT NULL UNIQUE,
+  [StudentID] INT NOT NULL UNIQUE,
   [StatusID] INT NOT NULL DEFAULT 2,
   [ApplicationDate] DATE NOT NULL DEFAULT GETDATE()
 )
