a744cf1c6b90c20bf39baee51794b243d27c84f2
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-10 12:19:49 +0200

    HeadOfDepartment, StudentBursaryApplication, and StudentBursaryDocuments fix

--- BursaryDB/Tables/ApplicationStatus.sql
+++ BursaryDB/Tables/ApplicationStatus.sql
@@ -3,3 +3,4 @@ CREATE TABLE [dbo].[ApplicationStatus]
   [StatusID] INT IDENTITY NOT NULL PRIMARY KEY,
   [Status] VARCHAR(10) NOT NULL DEFAULT 'Review'
 );
+GO
--- BursaryDB/Tables/StudentBursaryApplication.sql
+++ BursaryDB/Tables/StudentBursaryApplication.sql
@@ -4,17 +4,21 @@ CREATE TABLE [dbo].[StudentBursaryApplication]
   [ApplicationMotivation] TEXT NOT NULL,
   [ApplicationRejectionReason] TEXT DEFAULT '',
   [BursaryAmount] MONEY NOT NULL,
-  [StudentID] VARCHAR(13) NOT NULL UNIQUE,
+  [ApplicationDate] DATE NOT NULL,
+  [StudentID] INT NOT NULL UNIQUE,
   [StatusID] INT NOT NULL
 )
 GO
 
+
 ALTER TABLE [dbo].[StudentBursaryApplication] 
   ADD CONSTRAINT [FK_StudentBursaryApplication_Student_StudentID]
       FOREIGN KEY ([StudentID])
-      REFERENCES [dbo].[Student]([StudentID]),
+      REFERENCES [dbo].[Student]([StudentID])
+GO
 
-      CONSTRAINT [FK_StudentBursaryApplication_ApplicationStatus_StatusID]
+ALTER TABLE [dbo].[StudentBursaryApplication] 
+  ADD CONSTRAINT [FK_StudentBursaryApplication_ApplicationStatus_StatusID]
       FOREIGN KEY ([StatusID])
       REFERENCES [dbo].[ApplicationStatus]([StatusID])
 GO
