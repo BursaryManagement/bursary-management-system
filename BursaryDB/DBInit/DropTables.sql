c0d62205b0bd1ff175e48619c7beb4ad8e979dbb
Manqoba-stack <manyoba1997@gmail.com>
2024-02-11 14:57:55 +0200

    added removal of checkyearly

--- BursaryDB/DBInit/DropTables.sql
+++ BursaryDB/DBInit/DropTables.sql
@@ -123,7 +123,7 @@ GO
 IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[YearlyUniversityAllocation]') AND type in (N'U'))
 BEGIN
  ALTER TABLE [dbo].[YearlyUniversityAllocation]
-  DROP CONSTRAINT [FK_YearlyUniversityAllocation_University_UniversityID]
+  DROP CONSTRAINT  [CHK_YearlyUniversityAllocation_AllocatedAmount]
 END
 GO
 
