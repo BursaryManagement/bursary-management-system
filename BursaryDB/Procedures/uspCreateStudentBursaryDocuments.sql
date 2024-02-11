e6924f06a8160dd2688a75b1cf8a5c7fd3982236
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-11 10:07:26 +0200

    Updated uspCreateStudentBursaryDocuments to convert files to binary before saving them

--- BursaryDB/Procedures/uspCreateStudentBursaryDocuments.sql
+++ BursaryDB/Procedures/uspCreateStudentBursaryDocuments.sql
@@ -1,11 +1,23 @@
 CREATE PROCEDURE [dbo].[uspCreateStudentBursaryDocuments]
   @ApplicationID INT,
-  @IdCopy VARBINARY(MAX),
-  @Transcript VARBINARY(MAX),
-  @CurriculumVitae VARBINARY(MAX)
+  @IdCopyLoc NVARCHAR(200),
+  @TranscriptLoc NVARCHAR(200),
+  @CurriculumVitaeLoc NVARCHAR(200)
 AS
 BEGIN TRY
   BEGIN TRANSACTION
+    DECLARE @IdCopy VARBINARY(MAX)
+    SELECT @IdCopy = BulkColumn
+    FROM OPENROWSET(BULK ''' + @IdCopyLoc + ''', SINGLE_BLOB) AS Document;
+
+    DECLARE @Transcript VARBINARY(MAX)
+    SELECT @Transcript = BulkColumn
+    FROM OPENROWSET(BULK ''' + @TranscriptLoc + ''', SINGLE_BLOB) AS Document;
+
+    DECLARE @CurriculumVitae VARBINARY(MAX)
+    SELECT @CurriculumVitae = BulkColumn
+    FROM OPENROWSET(BULK ''' + @CurriculumVitaeLoc + ''', SINGLE_BLOB) AS Document;
+
     INSERT INTO [dbo].[StudentBursaryDocuments] (
       [ApplicationID],
       [IdCopy],
