731dd9e3b21341187af282c2322b9d5c39170f99
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-11 15:44:48 +0200

    add functionality to retrive status

---
+++ BursaryDB/ScalarFunctions/udfGetStatusName.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION [dbo].[udfGetStatusName]
+  (
+    @StatusID INT
+  )
+RETURNS VARCHAR(10)
+AS
+  BEGIN
+    DECLARE @StatusName VARCHAR(10)
+    SELECT @StatusName = [Status]
+    FROM [dbo].[ApplicationStatus]
+    WHERE [StatusID] = @StatusID
+    RETURN @StatusName
+  END
