563c69aacf185d5db52ca0667191f55fcd429110
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-11 16:14:08 +0200

    add function to retrieve student application status

---
+++ BursaryDB/ScalarFunctions/udfGetStudentApplicationStatusID.sql
@@ -0,0 +1,13 @@
+ALTER FUNCTION [dbo].[udfGetStudentApplicationStatusID]
+  (
+    @ApplicationID INT
+  )
+RETURNS INT
+AS
+  BEGIN
+    DECLARE @StatusID INT
+    SELECT @StatusID = [StatusID]
+    FROM [dbo].[StudentBursaryApplication]
+    WHERE [ApplicationID] = @ApplicationID
+    RETURN @StatusID
+END
