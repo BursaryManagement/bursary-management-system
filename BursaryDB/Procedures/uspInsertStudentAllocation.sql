ec7ae45ab1f82781789171c0a417313f4870e45b
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-12 07:55:40 +0200

    add procedure to process yearly allocations

---
+++ BursaryDB/Procedures/uspInsertStudentAllocation.sql
@@ -0,0 +1,12 @@
+ALTER PROCEDURE [dbo].[uspInsertStudentAllocation]
+	@ApplicationID INT
+AS
+	  DECLARE @StudentID INT
+	  DECLARE @BursaryAmount MONEY
+
+	  SELECT @StudentID = [dbo].[udfGetStudentIdFromApplication](@ApplicationID)
+	  
+	  SELECT @BursaryAmount = [dbo].[udfGetStudentBursaryAmount](@StudentID)
+
+	  EXEC [dbo].[uspAddStudentYearlyAllocation] @StudentID, @BursaryAmount
+GO
