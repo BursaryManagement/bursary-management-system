d1b447669c3af689662b9679b2eb71fa5d8b6214
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-12 08:28:09 +0200

    add a view to check students' application in review

--- BursaryDB/Views/vStudentsInReview.sql
+++ BursaryDB/Views/vStudentsInReview.sql
@@ -1,2 +1,15 @@
 CREATE VIEW [dbo].[vStudentsInReview]
-  AS SELECT * FROM [SomeTableOrView]
+  AS
+	SELECT [User].[FirstName], [User].[LastName], [StudentBursaryApplication].[BursaryAmount], [University].[UniversityName], [ApplicationStatus].[Status]
+	FROM [User]
+	INNER JOIN [Student]
+	ON [Student].[UserID] = [User].[UserID]
+	INNER JOIN StudentBursaryApplication
+	ON [Student].[StudentID] = [StudentBursaryApplication].[StudentID]
+	INNER JOIN [ApplicationStatus]
+	ON
+	[StudentBursaryApplication].[StatusID] = [ApplicationStatus].[StatusID]
+ INNER JOIN [University]
+ ON [Student].[UniversityID] = [University].[UniversityID]
+	WHERE [StudentBursaryApplication].[StatusID] = 2;
+GO
