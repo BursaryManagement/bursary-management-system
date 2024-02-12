6b745241a0e193b02aa64d37b49f46d5f8bb2d55
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-12 07:44:35 +0200

    add procedure to add student amount allocation to the allocation table

---
+++ BursaryDB/Procedures/uspAddStudentAllocationAmount.sql
@@ -0,0 +1,21 @@
+CREATE  PROCEDURE [dbo].[uspAddStudentYearlyAllocation]
+  @StudentID INT,
+  @AllocatedAmount MONEY
+ 
+AS
+  BEGIN TRY
+    DECLARE @StudentAllocated BIT
+    SELECT @StudentAllocated = [dbo].[udfCheckStudentAllocation](@StudentID)
+
+    IF(@StudentAllocated = 1)
+    BEGIN
+      RAISERROR('Student has been funded for this year', 16, 1)
+    END
+    INSERT INTO [dbo].[YearlyStudentAllocation]
+    ([StudentID], [AllocatedAmount], [DateAllocated])
+    VALUES
+    (@StudentID, @AllocatedAmount, GETDATE())
+  END TRY
+  BEGIN CATCH
+    PRINT ERROR_MESSAGE()
+  END CATCH
